03dbfb957e0a8f8bf17059c566fdead0d53d9ddb
Julie Ng <me@julie.io>
2025-12-30 18:59:11 +0100

    fix(header): columns

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -20,24 +20,26 @@ const links = [
 </script>
 
 <template>
-<div class="" style="background:var(--jng-primary-color);">
-  <UContainer class="py-3 flex items-center justify-between">
-    <div class="flex items-center">
-      <NuxtLink to="/" class="font-bold text-l text-white hover:text-emerald-300">
-        <img src="/images/logo-dark.svg" alt="Logo" class="mr-2">
-        AI Receipts POC
-      </NuxtLink>
-      <ul class="inline-block ml-5">
-        <li v-for="link, i in links" :key="i" class="inline-block mx-3">
-          <NuxtLink :to="link.href" class="text-slate-200 hover:text-emerald-300">
-            {{ link.text }}
-          </NuxtLink>
-        </li>
-      </ul>
-    </div>
-    <div class="flex text-white">
-      Hello {{ userFullName }}
-    </div>
+<div style="background:var(--jng-primary-color);">
+  <UContainer class="py-3">
+    <header class="flex items-center">
+      <div class="flex-1">
+        <NuxtLink to="/" class="font-bold text-l text-white hover:text-emerald-300">
+          <img src="/images/logo-dark.svg" alt="Logo" class="mr-2">
+          AI Receipts POC
+        </NuxtLink>
+        <ul class="inline-block ml-5">
+          <li v-for="link, i in links" :key="i" class="inline-block mx-3">
+            <NuxtLink :to="link.href" class="text-slate-200 hover:text-emerald-300">
+              {{ link.text }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </div>
+      <div class="text-white text-right">
+        Hello {{ userFullName }}
+      </div>
+    </header>
   </UContainer>
 </div>
 </template>
