44c280a8846d085ed251d77a9a6e48ea441939bc
Julie Ng <me@julie.io>
2026-01-04 14:29:59 +0100

    chore: fix indent

--- app/components/Upload/OverviewTabContent.vue
+++ app/components/Upload/OverviewTabContent.vue
@@ -13,38 +13,38 @@ const azureTags = computed(() => {
 <template>
 <div class="pt-6 px-4 flex flex-row">
   <div>
-  <h2 class="mb-3 font-semibold text-lg">File Info</h2>
-  <UIFileProperty label="Original Filename" :text="upload.originalFilename" />
-  <UIFileProperty label="Blob Size" :text="formatBytes(upload.size)" />
+    <h2 class="mb-3 font-semibold text-lg">File Info</h2>
+    <UIFileProperty label="Original Filename" :text="upload.originalFilename" />
+    <UIFileProperty label="Blob Size" :text="formatBytes(upload.size)" />
 
-  <h2 class="mt-6 mb-3 font-semibold text-lg">Azure Info</h2>
-  <UIFileProperty label="Blob Name" :text="upload.blobName" />
-  <UIFileProperty label="Blob Url" :text="upload.blobUrl" />
-  <ClientOnly>
-    <UIFileProperty label="Azure Blob Index Tags">
-      <div class="flex flex-wrap gap-2 pt-2">
-        <UBadge
-          v-for="tag in azureTags"
-          :key="tag.key"
-          class="text-slate-500"
-          color="neutral"
-          variant="soft"
-        >
-          {{ tag.key }}: {{ tag.value }}
-        </UBadge>
-      </div>
-    </UIFileProperty>
-  </ClientOnly>
-  <h2 class="mt-6 mb-3 font-semibold text-lg">OCR Analysis</h2>
-  <UIFileProperty label="Status" :text="upload.analysisStatus" />
-  <UIFileProperty v-if="upload.analyzedAt" label="Analyzed At" :text="timestampUtils.toShortDate(upload.analyzedAt)" />
-  <UIFileProperty v-if="upload.analyzedAt" label="OCR Result">
+    <h2 class="mt-6 mb-3 font-semibold text-lg">Azure Info</h2>
+    <UIFileProperty label="Blob Name" :text="upload.blobName" />
+    <UIFileProperty label="Blob Url" :text="upload.blobUrl" />
     <ClientOnly>
-      <pre class="w-fit mt-1 p-5 bg-slate-100 rounded-lg font-mono text-xs">
-        {{ upload.analysisOcrResult }}
-      </pre>
+      <UIFileProperty label="Azure Blob Index Tags">
+        <div class="flex flex-wrap gap-2 pt-2">
+          <UBadge
+            v-for="tag in azureTags"
+            :key="tag.key"
+            class="text-slate-500"
+            color="neutral"
+            variant="soft"
+          >
+            {{ tag.key }}: {{ tag.value }}
+          </UBadge>
+        </div>
+      </UIFileProperty>
     </ClientOnly>
-  </UIFileProperty>
+    <h2 class="mt-6 mb-3 font-semibold text-lg">OCR Analysis</h2>
+    <UIFileProperty label="Status" :text="upload.analysisStatus" />
+    <UIFileProperty v-if="upload.analyzedAt" label="Analyzed At" :text="timestampUtils.toShortDate(upload.analyzedAt)" />
+    <UIFileProperty v-if="upload.analyzedAt" label="OCR Result">
+      <ClientOnly>
+        <pre class="w-fit mt-1 p-5 bg-slate-100 rounded-lg font-mono text-xs">
+          {{ upload.analysisOcrResult }}
+        </pre>
+      </ClientOnly>
+    </UIFileProperty>
   </div>
   <div>
     <UploadImagePreview :blobName="upload.blobName" :alt="upload.blobName" />
