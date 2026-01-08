6a2bc236fb21e4cb1a4d7cd27e72bb9ea1187044
Julie Ng <me@julie.io>
2026-01-08 18:12:03 +0100

    fix(nav): centering and overflow when collapsed

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -9,40 +9,28 @@ const userId = store.userId
 const route = useRoute()
 // const toast = useToast()
 
-const open = ref(false)
+const collapsed = ref(false)
 const links = [
   {
     label: 'Home',
     icon: 'i-lucide-house',
     to: '/',
-    onSelect: () => {
-      open.value = false
-    }
   },
   {
     label: 'Azure AI Debug',
     icon: 'i-lucide-flask-conical',
     to: '/debug-analysis',
-    onSelect: () => {
-      open.value = false
-    },
   },
   {
     label: 'Blobs',
     icon: 'i-lucide-blocks',
     to: '/blobs',
-    onSelect: () => {
-      open.value = false
-    },
   },
   {
     label: 'Uploads',
     icon: 'i-lucide-upload-cloud',
     to: '/uploads',
     defaultOpen: true,
-    onSelect: () => {
-      open.value = false
-    },
     children: [
       {
         label: 'List',
@@ -56,9 +44,6 @@ const links = [
         label: 'New Upload',
         icon: 'i-lucide-upload',
         to: '/uploads/new',
-        onSelect: () => {
-          open.value = false
-        },
       }
     ],
   },
@@ -68,10 +53,10 @@ const links = [
 <template>
   <UDashboardGroup>
     <UDashboardSidebar id="default"
-      v-model:open="open"
       :default-size="12"
       :min-size="12"
       :max-size="15"
+      v-model:collapsed="collapsed"
       collapsible
       resizable
       class="bg-elevated/25"
@@ -79,9 +64,9 @@ const links = [
 
       <!-- Header -->
       <template #header="{ collapsed }">
-        <NuxtLink class="flex items-center px-2.5" to="/">
-          <UIcon name="i-lucide-scan-barcode" class="size-6  bg-blue-600" />
-          <div class="px-2 font-semibold text-slate-800">
+        <NuxtLink class="flex items-center" :class="{ 'py-1 px-1 bg-blue-600 rounded-sm': collapsed }" to="/">
+          <UIcon name="i-lucide-scan-barcode" class="size-6" :class="{ 'bg-white': collapsed }" />
+          <div v-if="!collapsed" class="px-2 font-semibold text-slate-800">
             Receipts AI
           </div>
         </NuxtLink>
@@ -94,11 +79,11 @@ const links = [
 
       <!-- User Icon -->
       <template #footer="{ collapsed }">
-        <div class="flex items-center px-2.5 py-1">
+        <div class="flex items-center py-1" :class="{ 'px-1': collapsed }">
           <div class="inline-flex p-1 items-center justify-center rounded-full bg-slate-200">
             <UIcon name="i-lucide-user-round" class="size-4 text-slate-400 bg-slate-500" />
           </div>
-          <div class="pl-2 text-sm text-slate-500">
+          <div v-if="!collapsed" class="pl-2 text-sm text-slate-500">
             {{ userId }}
           </div>
         </div>
