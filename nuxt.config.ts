e4316c55e9109191b7ba1b2552c51a6bc4cf4fae
Julie Ng <me@julie.io>
2026-01-08 13:06:28 +0100

    fix(dev): hmr for shared utils

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@pinia/nuxt', '@nuxthub/core', 'nuxt-shiki'],
   plugins: ['@/plugins/vue-json-pretty'],
+  watch: [
+    '~~/shared/**/*'
+  ],
   hub: {
     db: 'sqlite'
   },
