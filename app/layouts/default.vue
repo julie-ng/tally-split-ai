a01df836b25bdf0cce493365f0c14af68cee301d
Julie Ng <me@julie.io>
2026-04-06 10:36:33 +0200

    ui(navbar): list months under split, hardcode for now

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -15,6 +15,41 @@ function isActive (path: string) {
   return route.path === path || route.path.startsWith(path + '/')
 }
 
+const splitMonths = [
+  {
+    label: 'April 2026',
+    to: '/splits/2026/04/',
+  },
+  {
+    label: 'March 2026',
+    to: '/splits/2026/03/',
+  },
+  {
+    label: 'February 2026',
+    to: '/splits/2026/02/',
+  },
+  {
+    label: 'January 2026',
+    to: '/splits/2026/01/',
+  },
+  {
+    label: 'December 2025',
+    to: '/splits/2025/12/',
+  },
+  {
+    label: 'November 2025',
+    to: '/splits/2025/11/',
+  },
+  {
+    label: 'October 2025',
+    to: '/splits/2025/10/',
+  },
+  {
+    label: 'September 2025',
+    to: '/splits/2025/09/',
+  },
+]
+
 const links = computed(() => [
   [
     {
@@ -35,6 +70,8 @@ const links = computed(() => [
       icon: 'i-lucide-coins',
       to: '/splits',
       active: isActive('/splits'),
+      children: splitMonths,
+      defaultOpen: true,
     },
     {
       label: 'Uploads',
