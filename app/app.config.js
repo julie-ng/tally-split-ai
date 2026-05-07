ebebed2a7f758a8f23b5cb7b0d36f9bcbb801b4b
Julie Ng <me@julie.io>
2026-05-07 16:27:10 +0200

    ui(colors): change warning default

--- app/app.config.js
+++ app/app.config.js
@@ -4,6 +4,7 @@ export default defineAppConfig({
       primary: 'blue',
       secondary: 'teal',
       neutral: 'slate',
+      warning: 'amber',
     },
     container: {
       base: 'w-full max-w-full mx-auto px-4 sm:px-6 lg:px-8',
