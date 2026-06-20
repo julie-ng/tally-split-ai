50d369cf4fe2a802a92b355d986b0f55d848d052
Julie Ng <me@julie.io>
2026-06-20 07:40:16 +0200

    home(footer): add links to privacy, impressum

---
+++ app/components/homepage/footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-xs text-dimmed mt-12 mb-20 flex flex-items gap-6 justify-center">
+    <span>
+      Copyright 2025-2026 Julie Ng.
+    </span>
+
+    <NuxtLink to="/privacy" class="hover:underline">
+      Privacy
+    </NuxtLink>
+
+    <NuxtLink to="/impressum" class="hover:underline">
+      Impressum
+    </NuxtLink>
+  </div>
+</template>
