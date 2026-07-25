c69408cc1eede8f3622ec16188a47e652034f932
Julie Ng <me@julie.io>
2026-07-25 15:14:53 +0200

    chore: remove unused code

--- app/stores/uploads.store.js
+++ app/stores/uploads.store.js
@@ -31,13 +31,6 @@ export const useUploadsStore = defineStore('uploads', () => {
 
   const getPolygonsById = computed(() => id => polygons.value[id] ?? null)
 
-  const getJsonLinksById = computed(() => id => [
-    { label: 'Summary', href: `/api/analysis/summary/${id}` },
-    { label: 'Annotations', href: `/api/uploads/${id}/annotations` },
-    { label: 'OCR', href: `/api/uploads/${id}/ocr` },
-    { label: 'Polygons', href: `/api/uploads/${id}/polygons` },
-  ])
-
   // -------- ACTIONS --------
 
   /**
@@ -263,7 +256,6 @@ export const useUploadsStore = defineStore('uploads', () => {
     totalUploads,
     getUploadById,
     getPolygonsById,
-    getJsonLinksById,
 
     // Actions
     fetchUploads,
